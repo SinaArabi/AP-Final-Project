ccbf039c5d7dddc10f554356c83aa58fed9a8298
Sina Arabi <m.sina.arabi@gmail.com>
2022-05-27 11:20:01 +0430

    images fit, styling is left

--- reddit_sos/lib/main.dart
+++ reddit_sos/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import './tabs_screen.dart';
-
+import './subReddit.dart';
+import './subRedditPage.dart';
 
 void main() {
   runApp(defaultPage());
 }
+
 class defaultPage extends StatelessWidget {
   const defaultPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'defaultPage',
-    theme: ThemeData(primaryColor: Color.fromRGBO(66, 165, 245, 1.0),
-    accentColor: Color.fromARGB(49, 133, 207, 29),),
-    home: tabsScreen(),
+    return MaterialApp(
+      title: 'defaultPage',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(66, 165, 245, 1.0),
+        accentColor: Color.fromARGB(49, 133, 207, 29),
+      ),
+      home: subRedditPage(),
     );
   }
 }
