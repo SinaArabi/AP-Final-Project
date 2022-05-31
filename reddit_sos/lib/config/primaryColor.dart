9f3e0ab3ecdc6767a46489fc9b1ff2c3b149f7ab
Salar Jahanshiri <Salarjahanshiri@gmail.com>
2022-05-31 15:25:04 +0430

    "a line was added"

--- reddit_sos/lib/config/primaryColor.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/painting.dart';
-class PrimaryColor{
-  static const Color primary=Color.fromRGBO(106,50,159,1);
-}
--- reddit_sos/lib/main.dart
+++ reddit_sos/lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:reddit_sos/screens/login/login.dart';
-import './tabs_screen.dart';
+import 'package:reddit_sos/screens/welcome_screen.dart';
+import 'package:reddit_sos/screens/login_screen.dart';
+import 'package:reddit_sos/screens/registration_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(FlashChat());
 
-class MyApp extends StatelessWidget {
+class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-      title: 'Material App',
-      home: Scaffold(
-        backgroundColor: Colors.orange,
-        body: loginScreen(),
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
       ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id:(context)=>WelcomeScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        RegistrationScreen.id:(context)=>RegistrationScreen(),
+      },
     );
   }
 }
